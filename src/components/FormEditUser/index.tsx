603671b799ed46a5556a28ca055dad3683a2289d
Lucas Lopes <dev.thuram@gmail.com>
2021-03-25 17:43:07 -0300

    add input file on edit user

--- src/components/FormEditUser/index.tsx
+++ src/components/FormEditUser/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 
-import { InputText } from '../InputText'
+import { InputText } from 'components/InputText'
 import { InputSelect } from 'components/InputSelect'
+import { InputFile } from 'components/InputFile'
 import { Button } from '../Button'
 
 import styles from './styles.module.scss'
@@ -25,14 +26,45 @@ const GENDER_OPTIONS = [
   }
 ]
 
+const STATUS_OPTIONS = [
+  {
+    value: '0',
+    text: 'Desativado'
+  },
+  {
+    value: '1',
+    text: 'Ativo'
+  }
+]
+
 export function FormEditUser() {
   return (
     <form className={styles.wrapper}>
-      <InputText type="text" placeholder="Nome" />
+      <div className={styles.filesWrapper}>
+        <InputFile
+          name="profile"
+          label="Foto de perfil"
+          imageUrl="https://firebasestorage.googleapis.com/v0/b/bpr-dev.appspot.com/o/community%2Fuser%2F1234%20_thumb_1615330652.jpg?alt=media&token=230aaeff-75ef-4547-a199-625bd6dd6e10"
+        />
+        <InputFile
+          name="profile"
+          label="Comprovante (frente)"
+          imageUrl="https://firebasestorage.googleapis.com/v0/b/bpr-dev.appspot.com/o/community%2Fuser%2F1234%20_thumb_1615330652.jpg?alt=media&token=230aaeff-75ef-4547-a199-625bd6dd6e10"
+        />
+        <InputFile name="profile" label="Comprovante (verso)" />
+      </div>
 
-      <InputSelect options={GENDER_OPTIONS} placeholder="Selecionar gênero" />
+      <InputText type="text" placeholder="Nome" label="Nome completo" />
 
-      <InputText type="text" placeholder="Endereço" />
+      <InputSelect
+        options={GENDER_OPTIONS}
+        placeholder="Selecionar gênero"
+        label="Gênero"
+      />
+
+      <InputText type="text" placeholder="Endereço" label="Endereço" />
+
+      <InputSelect options={STATUS_OPTIONS} label="Status da conta" />
 
       <Button type="submit">Entrar</Button>
     </form>
