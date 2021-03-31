7b72c0b9cc7d54eab1267acfee6cf5d0a51c2ed1
Lucas Lopes <dev.thuram@gmail.com>
2021-03-31 17:02:08 -0300

    create table bikes

--- src/pages/communities/[id]/bikes/index.tsx
+++ src/pages/communities/[id]/bikes/index.tsx
@@ -1,5 +1,10 @@
 import { Base } from 'components/Base'
+import { TableBikes } from 'components/TableBikes'
 
 export default function Bikes() {
-  return <Base title="Bicicletas da comunidade: Aglomerado da Serra">Oi</Base>
+  return (
+    <Base title="Bicicletas da comunidade: Aglomerado da Serra">
+      <TableBikes />
+    </Base>
+  )
 }
