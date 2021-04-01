98fa3a8f19744b2bd6664aca3ee09240a3e176b6
Breno Farias <brenofarias87@yahoo.com.br>
2021-04-01 17:32:34 -0300

    feat(notification) : add notification abstraction

--- src/components/FormSignIn/index.tsx
+++ src/components/FormSignIn/index.tsx
@@ -1,12 +1,21 @@
 import { Button, Input, Stack } from '@chakra-ui/react'
+import React from 'react'
+import { useNotification } from '../../hooks/notification'
 
 export function FormSignIn() {
+  const erroCredenciais = useNotification({
+    message: 'Credenciais Inválidas.',
+    type: 'error'
+  })
+
   return (
     <form>
       <Stack spacing={4}>
         <Input type="email" placeholder="E-mail" />
         <Input type="password" placeholder="Password" />
-        <Button colorScheme="green">Entrar</Button>
+        <Button colorScheme="green" onClick={() => erroCredenciais()}>
+          Entrar
+        </Button>
       </Stack>
     </form>
   )
