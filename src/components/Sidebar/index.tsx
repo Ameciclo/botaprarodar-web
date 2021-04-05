cb07862fd079a3b1905fef352913f31e9636ce4c
Breno Farias <brenofarias87@yahoo.com.br>
2021-04-05 11:02:11 -0300

    add global colorscheme and logout component

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,30 +1,42 @@
-import { Link } from '@chakra-ui/react'
+import { Link, Image } from '@chakra-ui/react'
 import { Box, Flex, Heading, VStack } from '@chakra-ui/layout'
 import NextLink from 'next/link'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser, faGlobe } from '@fortawesome/free-solid-svg-icons'
 
 export function Sidebar() {
   return (
-    <Box
+    <Flex
       position="fixed"
+      flexDirection="column"
       left={0}
       p={4}
       w="200px"
       h="100%"
-      bg="gray.900"
-      borderRight="1px"
-      borderColor="green.500"
+      bg="#0f1a2e"
     >
-      <Flex alignItems="center" justifyContent="center">
+      <Flex
+        alignItems="center"
+        justifyContent="center"
+        backgroundColor="#012b39"
+      >
         <Heading color="white">Logo</Heading>
       </Flex>
-      <VStack mt={4} color="white">
+      <VStack mt={4} color="white" backgroundColor="#012b39">
         <NextLink href="/users">
-          <Link>Usuários</Link>
+          <Link>
+            <FontAwesomeIcon icon={faUser} /> Usuários
+          </Link>
         </NextLink>
         <NextLink href="/communities">
-          <Link>Comunidades</Link>
+          <Link>
+            <FontAwesomeIcon icon={faGlobe} /> Comunidades
+          </Link>
         </NextLink>
       </VStack>
-    </Box>
+      <Box mt="auto">
+        <Image objectFit="cover" src="/images/ameciclo.png" alt="Ameciclo" />
+      </Box>
+    </Flex>
   )
 }
