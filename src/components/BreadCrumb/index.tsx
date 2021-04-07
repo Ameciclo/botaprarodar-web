4d0f14a9abb440d0df95dc81286633643a5d7980
Lucas Lopes <dev.thuram@gmail.com>
2021-04-07 10:12:03 -0300

    configure jest and create first test

--- src/components/BreadCrumb/index.tsx
+++ src/components/BreadCrumb/index.tsx
@@ -2,53 +2,49 @@ import { Breadcrumb, BreadcrumbItem, BreadcrumbLink } from '@chakra-ui/react'
 import { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 
-const convertBreadcrumb = (string) => {
-  return string
-    .replace(/communities/g, 'comunidades')
-    .replace(/users/g, 'usuários')
-    .replace(/create/g, 'cadastro')
-    .replace(/(^\d)/, 'edição')
-    .toUpperCase()
+const formatPath = (path: string) => {
+  switch (path) {
+    case 'users':
+      return 'Usuários'
+    default:
+      return 'Edição'
+  }
+}
+
+type CrumbItemProps = {
+  page: string
+  href: string
 }
 
 export function BreadCrumb() {
-  const [breadcrumbs, setBreadcrumbs] = useState(null)
+  const [crumbs, setCrumbs] = useState<CrumbItemProps[]>([])
 
   const router = useRouter()
 
   useEffect(() => {
-    if (router) {
-      const linkPath = router.asPath.split('/')
-      linkPath.shift()
+    const linkPath = router.asPath.split('/').filter((value) => value !== '')
 
-      const pathArray = linkPath.map((path, i) => {
-        return {
-          breadcrumb: convertBreadcrumb(path),
-          href: '/' + linkPath.slice(0, i + 1).join('/')
-        }
-      })
+    const pathArray = linkPath.map((path, i) => {
+      return {
+        page: formatPath(path),
+        href: '/' + linkPath.slice(0, i + 1).join('/')
+      }
+    })
 
-      setBreadcrumbs(pathArray)
-    }
-  }, [router])
+    setCrumbs(pathArray)
+  }, [router.asPath])
 
-  if (!breadcrumbs) {
+  if (!crumbs.length) {
     return null
   }
 
   return (
-    <Breadcrumb ml="1em">
-      <BreadcrumbItem>
-        <BreadcrumbLink href="#">Início</BreadcrumbLink>
-      </BreadcrumbItem>
-
-      <BreadcrumbItem>
-        <BreadcrumbLink href="#">Docs</BreadcrumbLink>
-      </BreadcrumbItem>
-
-      <BreadcrumbItem isCurrentPage>
-        <BreadcrumbLink href="#">Breadcrumb</BreadcrumbLink>
-      </BreadcrumbItem>
+    <Breadcrumb ml="1em" role="navigation">
+      {crumbs.map(({ page, href }) => (
+        <BreadcrumbItem key={page}>
+          <BreadcrumbLink href={href}>{page}</BreadcrumbLink>
+        </BreadcrumbItem>
+      ))}
     </Breadcrumb>
   )
 }
--- src/components/BreadCrumb/test.tsx
+++ src/components/BreadCrumb/test.tsx
@@ -1,17 +1,37 @@
 import { BreadCrumb } from '.'
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const useRouter = jest.spyOn(require('next/router'), 'useRouter')
 
-describe('user', () => {
-  test('renders a message', () => {
+describe('<BreadCrumb />', () => {
+  it('should returns null if asPath is root', () => {
     useRouter.mockImplementation(() => ({
-      asPath: '/community'
+      asPath: '/'
     }))
 
-    const { container } = render(<BreadCrumb />)
+    render(<BreadCrumb />)
 
-    console.log(container)
+    expect(screen.queryByRole('navigation')).toBeNull()
+  })
+
+  it('should returns current pathname if not root', () => {
+    useRouter.mockImplementation(() => ({
+      asPath: '/users'
+    }))
+
+    render(<BreadCrumb />)
+
+    expect(screen.queryAllByRole('listitem').length).toBe(1)
+  })
+
+  it('should returns path params formatted', () => {
+    useRouter.mockImplementation(() => ({
+      asPath: '/users/1'
+    }))
+
+    render(<BreadCrumb />)
+
+    expect(screen.getByText(/usuários/i)).toHaveAttribute('href', '/users')
   })
 })
