389734867bb9d8ff6a39f9c81a01f1ca9509d7a7
analuizasb2 <analuizasb2@gmail.com>
2021-07-28 14:48:53 -0300

    Added community page to routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch } from 'react-router-dom';
+import CommunityPage from '../pages/CommunityPage/CommunityPage';
 import UserPage from '../pages/UserPage/UserPage';
 import Route from './Route';
 import TemplatePage from './templatePage';
@@ -7,7 +8,7 @@ import TemplatePage from './templatePage';
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/users" exact component={UserPage} />
-    <Route path="/comunities" component={TemplatePage} />
+    <Route path="/comunities" component={CommunityPage} />
   </Switch>
 );
 
