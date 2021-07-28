a6bd24cd7c882883d89079659cb9e2e1ca3311a4
analuizasb2 <analuizasb2@gmail.com>
2021-07-28 17:48:00 -0300

    Added test to render two communities

--- src/pages/CommunityPage/CommunityPage.test.tsx
+++ src/pages/CommunityPage/CommunityPage.test.tsx
@@ -1,15 +1,24 @@
 import CommunityPage from './CommunityPage';
 import { render, screen } from '@testing-library/react';
 
+const defaultCommunities: string[] = [];
+
 describe('CommunityPage', () => {
   it('renders communities page without error', () => {
-    const { container } = render(<CommunityPage />);
+    const { container } = render(<CommunityPage props={defaultCommunities} />);
     expect(container).toBeInTheDocument();
   });
 
   it('renders a list to show the communities', () => {
-    render(<CommunityPage />);
+    render(<CommunityPage props={defaultCommunities} />);
     const communitiesList = screen.getByTestId('communities-list');
     expect(communitiesList).toBeInTheDocument;
+  });
+
+  it('renders a list with two communities', () => {
+    const newCommunities = ['First Community', 'Second Community'];
+    render(<CommunityPage props={newCommunities} />);
+    const communitiesList = screen.getByTestId('communities-list');
+    expect(communitiesList).toHaveLength(2);
   });
 });
--- src/pages/CommunityPage/CommunityPage.tsx
+++ src/pages/CommunityPage/CommunityPage.tsx
@@ -1,11 +1,17 @@
 import React, { useState } from 'react';
+interface Communities {
+  Community: {
+    name: string;
+  }[];
+}
 
-const CommunityPage: React.FC = () => {
-  //  const [communities, setCommunities] = useState([]);
+const CommunityPage: React.FC<{ props: string[] }> = ({ props }) => {
+  const [communities, setCommunities] = useState<Communities>();
+
   return (
     <div>
       <ul data-testid="communities-list">
-        <li>Communitie 1</li>
+        <li>Community 1</li>
       </ul>
     </div>
   );
