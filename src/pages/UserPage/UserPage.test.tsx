26694e2d49c4b0beb9e351565729b193fbf51261
Beatriz Vilalta <beatriz.vilalta@thoughtworks.com>
2021-08-03 10:43:40 -0300

    feat: administrador-listar-usuarios - added UserService tests

--- src/pages/UserPage/UserPage.test.tsx
+++ src/pages/UserPage/UserPage.test.tsx
@@ -2,26 +2,38 @@
 // O que eu preciso saber? Listar os usuários
 // O que eu espero? Uma tabela com a lista de usuários contento seu nome, e-mail, comunidade, status e ações
 import { render, screen } from '@testing-library/react';
+import UserService from '../../services/UserService/UserService';
 import UserPage from './UserPage';
 
 describe('UserPage', () => {
   beforeEach(() => {
-    jest.mock('../../services/UserService/UserService', () => {
-      return {
-        default: {
-          get: jest.fn(() =>
-            Promise.resolve([
-              {
-                name: 'Antoni',
-                communityId: '-MLDOXs3p35DEHg0gdUU',
-                telephone: '+55 51 3626-2001',
-                status: true,
-              },
-            ]),
-          ),
-        },
-      };
-    });
+    jest.spyOn(UserService, 'getAllUsers').mockResolvedValue([
+      {
+        name: 'Antoni',
+        communityId: '-MLDOXs3p35DEHg0gdUU',
+        telephone: '+55 51 3626-2001',
+        status: true,
+      },
+    ]);
+    // jest.mock('firebase/app', () => {
+    //   const data = [
+    //     {
+    //       name: 'Antoni',
+    //       communityId: '-MLDOXs3p35DEHg0gdUU',
+    //       telephone: '+55 51 3626-2001',
+    //       status: true,
+    //     },
+    //   ];
+    //   const snapshot = { val: () => data };
+    //   return {
+    //     initializeApp: jest.fn().mockReturnValue({
+    //       database: jest.fn().mockReturnValue({
+    //         ref: jest.fn().mockReturnThis(),
+    //         once: jest.fn(() => Promise.resolve(snapshot)),
+    //       }),
+    //     }),
+    //   };
+    // });
   });
 
   it('should list users', async () => {
