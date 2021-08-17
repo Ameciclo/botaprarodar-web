cb495f3f582242fbfe4eed1888cab1192a5c7eba
arycleniobarros <arycleniobarros@gmail.com>
2021-08-17 14:59:18 -0300

    feat: Implementing heading of user detail

--- src/pages/UserDetailPage/UserDetailPage.styles.ts
+++ src/pages/UserDetailPage/UserDetailPage.styles.ts
@@ -3,35 +3,19 @@ import { makeStyles } from '@material-ui/core';
 export default makeStyles(() => ({
   root: {
     margin: '0px 15px',
-    height: '88vh',
   },
   heading: {
     fontFamily: 'Roboto',
     fontStyle: 'normal',
     fontWeight: 'normal',
     fontSize: 24,
-    color: '#515151',
     letterSpacing: '0.18px',
     '& a': {
       color: '#515151',
       textDecoration: 'none',
+      '&:hover': {
+        color: '#616161',
+      },
     },
   },
-  avatar: {
-    width: '118px !important',
-    height: '118px !important',
-    display: 'block',
-    margin: 'auto',
-    marginBottom: '16px',
-  },
-  username: {
-    fontFamily: 'Roboto',
-    fontStyle: 'normal',
-    fontWeight: 500,
-    fontSize: 20,
-    lineHeight: 24,
-    letterSpacing: '0.15px',
-    color: '#263238',
-  },
-  userContainer: {},
 }));
