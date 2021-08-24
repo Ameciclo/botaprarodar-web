ec3c895e1c691dcadce65d809f527faff0d8a362
Wendel Macedo <wendelmacedooliveira@hotmail.com>
2021-08-24 12:21:27 -0300

    feat(husky) Customizing changes options

--- .cz-config.js
+++ .cz-config.js
@@ -2,6 +2,7 @@ module.exports = {
   types: [
     { value: 'feat', name: 'feat:     A new feature' },
     { value: 'fix', name: 'fix:      A bug fix' },
+    { value: 'merge', name: 'merge:      Merging changes' },
     { value: 'docs', name: 'docs:     Documentation only changes' },
     {
       value: 'style',
@@ -26,29 +27,17 @@ module.exports = {
     { value: 'WIP', name: 'WIP:      Work in progress' },
   ],
 
-  scopes: [{ name: 'accounts' }, { name: 'admin' }, { name: 'exampleScope' }, { name: 'changeMe' }],
+  scopes: [{ name: 'dashboard' }, { name: 'login' }, { name: 'users' }, { name: 'communities' }],
 
   allowTicketNumber: false,
   isTicketNumberRequired: false,
   ticketNumberPrefix: 'TICKET-',
   ticketNumberRegExp: '\\d{1,5}',
+  subjectSeparator: ' ',
 
-  // it needs to match the value for field type. Eg.: 'fix'
-  /*
-  scopeOverrides: {
-    fix: [
-      {name: 'merge'},
-      {name: 'style'},
-      {name: 'e2eTest'},
-      {name: 'unitTest'}
-    ]
-  },
-  */
-  // override the messages, defaults are as follows
   messages: {
     type: "Select the type of change that you're committing:",
     scope: '\nDenote the SCOPE of this change (optional):',
-    // used if allowCustomScopes is true
     customScope: 'Denote the SCOPE of this change:',
     subject: 'Write a SHORT, IMPERATIVE tense description of the change:\n',
     body: 'Provide a LONGER description of the change (optional). Use "|" to break new line:\n',
@@ -59,12 +48,7 @@ module.exports = {
 
   allowCustomScopes: true,
   allowBreakingChanges: ['feat', 'fix'],
-  // skip any questions you want
-  skipQuestions: ['body'],
-
-  // limit subject length
+  skipQuestions: ['body', 'footer'],
+  upperCaseSubject: true,
   subjectLimit: 100,
-  // breaklineChar: '|', // It is supported for fields body and footer.
-  // footerPrefix : 'ISSUES CLOSED:'
-  // askForBreakingChangeFirst : true, // default is false
 };
