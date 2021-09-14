355403053f13e0013bd9976ce5b0e179eff59033
analuizasb2 <analuizasb2@gmail.com>
2021-09-14 15:49:13 -0300

    chore: Forcing deploy to heroku

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders App without error', () => {
+test('renders without error', () => {
   const { container } = render(<App />);
   expect(container).toBeInTheDocument();
 });
