6af98ba761aa3b3142eb24b3a024a795dc184509
giovanne-santos05 <giovanne.santos@thoughtworks.com>
2022-08-02 18:23:04 -0300

    merge: Fix: Fixed import into index shared components

--- src/shared/components/index.tsx
+++ src/shared/components/index.tsx
@@ -1,6 +1,7 @@
 export { default as Input } from './Input/Input';
 export { default as Loading } from './Loading/Loading';
 export { default as Menu } from './Menu/Menu';
+export { default as CustomLabel } from './CustomLabel/CustomLabel';
 export { default as toast, ToastContainer } from './Toast/Toast';
 export { default as Icon } from './Icon/Icon';
 export { default as PageTitle } from './PageTitle/PageTitle';
