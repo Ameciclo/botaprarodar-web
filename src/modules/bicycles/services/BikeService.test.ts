c1e797e24970a13aadda16fe2d80eba89ecf7c11
giovanne-santos05 <giovanne.santos@thoughtworks.com>
2022-08-03 17:23:45 -0300

    merge: Test(bike): Added test on bike service about bike count and when api fails

--- src/modules/bicycles/services/BikeService.test.ts
+++ src/modules/bicycles/services/BikeService.test.ts
@@ -1,58 +1,140 @@
 import { act, waitFor } from '@testing-library/react';
 import BikeService from './BikeService';
 import api from '../../../shared/services/api';
-import {
-  MockedBikeOfXPTOCommunity,
-  MockedBikeOfTestCommunity,
-} from '../mocks/BikeMocks';
+import { mockedBikes, mockedBike } from '../mocks/BikeMocks';
 import Bike from '../models/Bike';
-import {
-  MockedFirstCommunity,
-  MockedSecondCommunity,
-} from 'modules/communities/mocks/MockedCommunity';
+import AmountBikesPerCommunity from '../utils/AmountBikesPerCommunity';
 
 jest.mock('shared/services/api');
 const mockedApi = api as jest.Mocked<typeof api>;
 
 const mockedApiBikesResponse = {
-  data: {
-    '-MgfYTcrUFsX_NXhhvL2': MockedBikeOfXPTOCommunity,
-    '-MLy8y1-5v5GLg7Z428y': MockedBikeOfTestCommunity,
-  },
+  data: [mockedBike(), mockedBike(), mockedBike(), mockedBike(), mockedBike()],
 };
-const mockedBikes = [MockedBikeOfXPTOCommunity, MockedBikeOfTestCommunity];
 
 describe('Bike Service', () => {
-  it('should get all bikes', async () => {
-    mockedApi.get.mockResolvedValue(mockedApiBikesResponse);
-    let data: Bike[];
+  describe('when api works', () => {
+    it('should get all bikes', async () => {
+      mockedApi.get.mockResolvedValue(mockedApiBikesResponse);
+      let data: Bike[];
 
-    await act(async () => {
-      data = await BikeService.getAllBikes();
-    });
+      await act(async () => {
+        data = await BikeService.getAllBikes();
+      });
 
-    expect(mockedApi.get).toHaveBeenCalledWith('/bikes.json');
-    await waitFor(() => expect(data).toStrictEqual(mockedBikes));
-  });
-
-  it.skip('should get all bikes of Comunidade XPTO', async () => {
-    mockedApi.get.mockResolvedValue(mockedApiBikesResponse);
-    let amount: number;
-
-    await act(async () => {
-      amount = await BikeService.getAmountFilteredBikesPerCommunity(
-        MockedFirstCommunity.id,
+      expect(mockedApi.get).toHaveBeenCalledWith('/bikes.json');
+      await waitFor(() =>
+        expect(data).toStrictEqual(mockedApiBikesResponse.data),
       );
     });
 
-    const filteredMockedBikesPerCommunity = mockedBikes.filter(bike => {
-      if (bike.communityId === MockedFirstCommunity.id) {
-        return bike;
-      }
-    });
+    describe('getAmountFilteredBikesPerCommunity', () => {
+      it('should get all bikes of same community', async () => {
+        mockedApi.get.mockResolvedValue(mockedApiBikesResponse);
+        let amount: AmountBikesPerCommunity;
 
-    await waitFor(() =>
-      expect(amount).toEqual(filteredMockedBikesPerCommunity.length),
-    );
+        await act(async () => {
+          amount = await BikeService.getAmountFilteredBikesPerCommunity(
+            '-MLDOXs3p35DEHg0gdUU',
+          );
+        });
+
+        await waitFor(() =>
+          expect(amount).toEqual({ total: 5, available: 0, borrowed: 5 }),
+        );
+      });
+
+      it('should get all bikes in a community without bikes', async () => {
+        mockedApiBikesResponse.data = [
+          mockedBike({ communityId: '-MLy8y1-5v5GLg7Z428y' }),
+          mockedBike({ communityId: '-MLy8y1-5v5GLg7Z428y' }),
+          mockedBike({ communityId: '-MLy8y1-5v5GLg7Z428y' }),
+        ];
+        mockedApi.get.mockResolvedValue(mockedApiBikesResponse);
+        let amount: AmountBikesPerCommunity;
+
+        await act(async () => {
+          amount = await BikeService.getAmountFilteredBikesPerCommunity(
+            '-MLDOXs3p35DEHg0gdUU',
+          );
+        });
+
+        await waitFor(() =>
+          expect(amount).toEqual({ total: 0, available: 0, borrowed: 0 }),
+        );
+      });
+
+      it('should get all bikes in a different communities', async () => {
+        mockedApiBikesResponse.data = [
+          mockedBike({ communityId: '-MLy8y1-5v5GLg7Z428y' }),
+          mockedBike({ communityId: '-MLy8y1-5v5GLg7Z428y' }),
+          mockedBike({ communityId: '-MLDOXs3p35DEHg0gdUU' }),
+          mockedBike({ communityId: '-MLDOXs3p35DEHg0gdUU' }),
+        ];
+        mockedApi.get.mockResolvedValue(mockedApiBikesResponse);
+        let amount: AmountBikesPerCommunity;
+
+        await act(async () => {
+          amount = await BikeService.getAmountFilteredBikesPerCommunity(
+            '-MLDOXs3p35DEHg0gdUU',
+          );
+        });
+
+        await waitFor(() =>
+          expect(amount).toEqual({ total: 2, available: 0, borrowed: 2 }),
+        );
+      });
+
+      it('should get all bikes in a different communities with available bikes', async () => {
+        mockedApiBikesResponse.data = [
+          mockedBike({ communityId: '-MLy8y1-5v5GLg7Z428y' }),
+          mockedBike({ communityId: '-MLy8y1-5v5GLg7Z428y', available: true }),
+          mockedBike({ communityId: '-MLDOXs3p35DEHg0gdUU', available: true }),
+          mockedBike({ communityId: '-MLDOXs3p35DEHg0gdUU' }),
+        ];
+        mockedApi.get.mockResolvedValue(mockedApiBikesResponse);
+        let amount: AmountBikesPerCommunity;
+
+        await act(async () => {
+          amount = await BikeService.getAmountFilteredBikesPerCommunity(
+            '-MLDOXs3p35DEHg0gdUU',
+          );
+        });
+
+        await waitFor(() =>
+          expect(amount).toEqual({ total: 2, available: 1, borrowed: 1 }),
+        );
+      });
+
+      it('should return default values when dont have bikes', async () => {
+        mockedApiBikesResponse.data = [];
+        mockedApi.get.mockResolvedValue(mockedApiBikesResponse);
+        let amount: AmountBikesPerCommunity;
+
+        await act(async () => {
+          amount = await BikeService.getAmountFilteredBikesPerCommunity(
+            '-MLDOXs3p35DEHg0gdUU',
+          );
+        });
+
+        await waitFor(() =>
+          expect(amount).toEqual({ total: 0, available: 0, borrowed: 0 }),
+        );
+      });
+    });
+    describe('should return error when api fails', async () => {
+      it('should return error', async () => {
+        const err = new Error('Error de comunicação com a api');
+        mockedApi.get.mockRejectedValueOnce(err);
+
+        try {
+          await BikeService.getAmountFilteredBikesPerCommunity(
+            '-MLDOXs3p35DEHg0gdUU',
+          );
+        } catch (e) {
+          expect(e).toEqual(err);
+        }
+      });
+    });
   });
 });
