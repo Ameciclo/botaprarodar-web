f5c10c0db5e72e42fc18aab24f5a9a5d9ed6f22f
Alexsandro Alves <alexsandro.alves@Alexsandros-MacBook-Pro.local>
2022-08-03 13:58:44 -0300

    merge: Test: Add test

--- src/shared/utils/ColorsUtils.ts
+++ src/shared/utils/ColorsUtils.ts
@@ -1,56 +1,26 @@
-const getPastelColor = (opacity = 1) => {
-  const hue = Math.floor(Math.random() * 12) * 30;
-  const randomColor = `hsla(${hue}, 70%, 80%, ${opacity})`;
-  return randomColor;
-};
-
-const getColorArray = (length: number) => {
-  const array: string[] = [];
-
-  for (let i = 0; i < length; i += 1) {
-    const newColor = getPastelColor();
-    if (array.includes(newColor)) {
-      i -= 1;
-    } else {
-      array.push(newColor);
-    }
-  }
-  return array;
-};
-
-/* 
-  
-*/
-
-/**
- * Returns two arrays of color.
-  
- * The first one with 0.3 of opacity and the second with 1 of opacity.
- 
- * Both arrays are in 'hsla' color
-
- * @param length Length of the array
- */
-const getColorArrays = (length: number) => {
-  const array1: string[] = [];
-  const array2: string[] = [];
-
-  for (let i = 0; i < length; i += 1) {
-    const newColor = getPastelColor(0.3);
-    if (array1.includes(newColor)) {
-      i -= 1;
-    } else {
-      array1.push(newColor);
-      array2.push(newColor.replace('0.3', '1'));
-    }
-  }
-  return [array1, array2];
-};
-
 const ColorsUtils = {
-  getPastelColor,
-  getColorArray,
-  getColorArrays,
+  getPastelColor(opacity = 1) {
+    const hue = Math.floor(Math.random() * 12) * 30;
+    const randomColor = `hsla(${hue}, 70%, 80%, ${opacity})`;
+
+    return randomColor;
+  },
+  getColorArrays(length: number) {
+    const array1: string[] = [];
+    const array2: string[] = [];
+
+    for (let i = 0; i < length; i += 1) {
+      const newColor = this.getPastelColor(0.3);
+      console.log(newColor);
+      if (array1.includes(newColor)) {
+        i -= 1;
+      } else {
+        array1.push(newColor);
+        array2.push(newColor.replace('0.3', '1'));
+      }
+    }
+    return [array1, array2];
+  },
 };
 
 export default ColorsUtils;
