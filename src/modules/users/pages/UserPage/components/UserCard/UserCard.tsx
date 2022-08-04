bd99926cb73d4b4c51091b1cd5d07465f9d25e24
Wallace Soares <>
2022-08-04 10:53:55 -0300

    refactor(users): Removing unsued code

--- src/modules/users/pages/UserPage/components/UserCard/UserCard.tsx
+++ src/modules/users/pages/UserPage/components/UserCard/UserCard.tsx
@@ -1,12 +1,13 @@
 import { Avatar, Card, CardContent, Typography, Grid } from '@material-ui/core';
-import {
-  AssignmentOutlined,
-  LockOutlined,
-  PlaceOutlined,
-} from '@material-ui/icons';
+import { LockOutlined, PlaceOutlined } from '@material-ui/icons';
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { toast } from 'shared/components';
+import { GenderTypes } from 'modules/users/models/types/GenderTypes';
+import { RacialTypes } from 'modules/users/models/types/RacialTypes';
+import { SchoolingTypes } from 'modules/users/models/types/SchoolingTypes';
+import { SchoolingStatusTypes } from 'modules/users/models/types/SchoolingStatusTypes';
+import { IncomeTypes } from 'modules/users/models/types/IncomeTypes';
 import UserMenu from '../UserMenu/UserMenu';
 import useStyles, { StyledBadgeUserCard } from './UserCard.styles';
 import UserService from '../../../../services/UserService';
@@ -14,14 +15,28 @@ import UserService from '../../../../services/UserService';
 interface UserCardProps {
   user: {
     name: string;
+    createDate: string;
+    address: string;
+    gender: GenderTypes;
+    profilePicture: string;
+    age: string;
+    racial: RacialTypes;
+    schooling: SchoolingTypes;
+    schoolingStatus: SchoolingStatusTypes;
+    income: IncomeTypes;
     communityId: string;
     telephone: string;
     status: boolean;
-    profilePicture: string;
     id: string;
-    address: string;
-    docNumber: bigint;
     isBlocked: boolean;
+    userQuiz: {
+      alreadyUseBPR: boolean;
+      alreadyUseBPROpenQuestion: string;
+      motivationOpenQuestion: string;
+      alreadyAccidentVictim: boolean;
+      problemsOnWayOpenQuestion: string;
+      timeOnWayOpenQuestion: string;
+    };
   };
 }
 
@@ -79,12 +94,6 @@ const UserCard: React.FC<UserCardProps> = ({ user, ...rest }) => {
             </Grid>
             <Grid item xs={8}>
               <Typography className={classes.name}>{displayName}</Typography>
-              <div className={classes.description}>
-                <AssignmentOutlined className={classes.icon} />
-                <Typography className={classes.text}>
-                  {user?.docNumber?.toString()}
-                </Typography>
-              </div>
               <div className={classes.description}>
                 <PlaceOutlined className={classes.icon} />
                 <Typography className={classes.text}>
