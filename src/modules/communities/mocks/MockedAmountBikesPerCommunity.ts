e3a117d81421c7be94554abb44a2e94185fcc9bb
giovanne-santos05 <giovanne.santos@thoughtworks.com>
2022-08-04 17:49:17 -0300

    merge: Refactor(communities): Removed desnecessary mock

--- src/modules/communities/mocks/MockedAmountBikesPerCommunity.ts
+++
@@ -1,7 +0,0 @@
-import AmountBikesPerCommunity from 'modules/bicycles/utils/AmountBikesPerCommunity';
-
-export const MockedAmountCommunityXPTO: AmountBikesPerCommunity = {
-  total: 1,
-  available: 1,
-  borrowed: 0,
-};
