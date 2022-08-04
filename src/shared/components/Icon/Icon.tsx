bd0afecaa091d786136c10733013a2e84460e492
giovanne-santos05 <giovanne.santos@thoughtworks.com>
2022-08-04 11:47:11 -0300

    merge: Merge: Perf: Added new icons on icon component

--- src/shared/components/Icon/Icon.tsx
+++ src/shared/components/Icon/Icon.tsx
@@ -1,7 +1,18 @@
 import { FC } from 'react';
-import { GearIcon } from 'shared/assets/icons';
+import {
+  GearIcon,
+  GiveBackBikeIcon,
+  LendBikeIcon,
+  RegisterBikeIcon,
+  RegisterUserIcon,
+} from 'shared/assets/icons';
 
-export type IconTypes = 'gear';
+export type IconTypes =
+  | 'gear'
+  | 'lendBike'
+  | 'giveBackBike'
+  | 'registerBike'
+  | 'registerUser';
 
 interface Props {
   name: IconTypes;
@@ -11,6 +22,34 @@ const Icon: FC<Props> = ({ name }) => {
   const iconOptions = {
     gear: (
       <img src={GearIcon} alt="Configurações" data-testid={`icon-${name}`} />
+    ),
+    lendBike: (
+      <img
+        src={LendBikeIcon}
+        alt="Emprestar bicicleta"
+        data-testid={`icon-${name}`}
+      />
+    ),
+    giveBackBike: (
+      <img
+        src={GiveBackBikeIcon}
+        alt="Devolver bicicleta"
+        data-testid={`icon-${name}`}
+      />
+    ),
+    registerBike: (
+      <img
+        src={RegisterBikeIcon}
+        alt="Cadastrar bicicleta"
+        data-testid={`icon-${name}`}
+      />
+    ),
+    registerUser: (
+      <img
+        src={RegisterUserIcon}
+        alt="Cadastrar usuária"
+        data-testid={`icon-${name}`}
+      />
     ),
   };
 
