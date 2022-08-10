53d3466889ca46b866d957c288d019b85202ceb8
Alexsandro Alves <alexsandro.alves@Alexsandros-MacBook-Pro.local>
2022-08-10 17:03:31 -0300

    chore: Update E2E config

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,24 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'cypress';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const DEFAULT_URL = 'http://localhost:3000/';
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'https://dev-botaprarodar.netlify.app/',
+    viewportWidth: 320,
+    viewportHeight: 600,
+    setupNodeEvents(on, config) {
+      /* eslint-disable no-param-reassign */
+      config.env = config.env || {};
+
+      config.baseUrl = process.env.REACT_APP_E2E_BASE_URL || DEFAULT_URL;
+      config.env.username = process.env.REACT_APP_E2E_USER;
+      config.env.password = process.env.REACT_APP_E2E_PASS;
+
+      return config;
+    },
   },
 });
