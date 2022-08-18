b56f7188cb15c3086b0029958210bbbf3c790698
Alexsandro Alves <alexsandro.alves@Alexsandros-MacBook-Pro.local>
2022-08-18 18:49:12 -0300

    test: CustomCardWithIcon

--- src/shared/components/CustomCardWithIcon/CustomCardWithIcon.test.tsx
+++ src/shared/components/CustomCardWithIcon/CustomCardWithIcon.test.tsx
@@ -1,18 +1,23 @@
 import { render, screen } from '@testing-library/react';
-import CustomCardWithIcon from './CustomCardWithIcon';
+import userEvent from '@testing-library/user-event';
+import CustomCardWithIcon, { Props } from './CustomCardWithIcon';
 
 jest.mock('../Icon/Icon', () => () => 'Icon-component-mock');
 
 describe('Component: CustomCardWithIcon', () => {
+  let defaultProps: Props;
+
+  beforeEach(() => {
+    defaultProps = {
+      id: 'my-id',
+      text: 'My Text',
+      iconName: 'giveBackBike',
+      iconDescription: 'Teste',
+    };
+  });
+
   it('should render correctly', () => {
-    render(
-      <CustomCardWithIcon
-        id="my-id"
-        text="My Text"
-        iconName="lendBike"
-        iconDescription="Teste"
-      />,
-    );
+    render(<CustomCardWithIcon {...defaultProps} />);
 
     const title = screen.getByRole('heading', { name: /My Text/i });
     const icon = screen.getByText('Icon-component-mock');
@@ -21,5 +26,17 @@ describe('Component: CustomCardWithIcon', () => {
     expect(title).toHaveAttribute('data-testid', 'my-id');
 
     expect(icon).toBeInTheDocument();
+  });
+
+  it('should render correctly with link', () => {
+    const onClick = jest.fn();
+
+    render(<CustomCardWithIcon {...defaultProps} onClick={onClick} />);
+
+    const { id } = defaultProps;
+    const action = screen.getByTestId(`card-action-${id}`);
+
+    userEvent.click(action);
+    expect(onClick).toHaveBeenCalled();
   });
 });
