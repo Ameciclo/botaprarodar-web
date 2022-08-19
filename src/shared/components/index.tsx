8291245a06cc97f7d7a605c93625c38c71a2a4c7
Alexsandro Alves <alexsandro.alves@Alexsandros-MacBook-Pro.local>
2022-08-19 14:56:53 -0300

    refactor: Add EmptyState and FormHeader in share/components

--- src/shared/components/index.tsx
+++ src/shared/components/index.tsx
@@ -6,3 +6,5 @@ export { default as toast, ToastContainer } from './Toast/Toast';
 export { default as Icon } from './Icon/Icon';
 export { default as PageTitle } from './PageTitle/PageTitle';
 export { default as Select } from './Select/Select';
+export { default as EmptyState } from './EmptyState/EmptyState';
+export { default as FormHeader } from './FormHeader/FormHeader';
