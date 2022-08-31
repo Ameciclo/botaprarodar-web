3384e936d5682d495932d8b0420106452da67d81
Wallace Soares <107143825+wallace-soares@users.noreply.github.com>
2022-08-31 08:58:15 -0300

    feat(dashboard): Switching display minutes to hours on the dashboard (#65)
    Co-authored-by: Wallace Soares <>

--- src/modules/dashboard/pages/DashboardPage/components/TravelTimeCard/TravelTimeCard.test.tsx
+++ src/modules/dashboard/pages/DashboardPage/components/TravelTimeCard/TravelTimeCard.test.tsx
@@ -1,13 +1,17 @@
 import { render, screen } from '@testing-library/react';
 import TravelTimeCard from './TravelTimeCard';
 
-it('should render travel time card', () => {
-  const mockTravelTime = [10, 20, 30.5557];
-  const { container } = render(<TravelTimeCard travelTime={mockTravelTime} />);
-  expect(container).toBeInTheDocument();
-  expect(screen.getByText('60.56')).toBeInTheDocument();
-  expect(screen.getByText('3')).toBeInTheDocument();
-  expect(
-    screen.getByText('Em média, são 20 minutos por viagem'),
-  ).toBeInTheDocument();
+describe('Travel Time Cards', () => {
+  it('should render travel time card', () => {
+    const mockTravelTime = [10, 20, 30.65];
+    const { container } = render(
+      <TravelTimeCard travelTime={mockTravelTime} />,
+    );
+    expect(container).toBeInTheDocument();
+    expect(screen.getByText('1.01')).toBeInTheDocument();
+    expect(screen.getByText('3')).toBeInTheDocument();
+    expect(
+      screen.getByText('Em média, são 0.33 horas por viagem'),
+    ).toBeInTheDocument();
+  });
 });
