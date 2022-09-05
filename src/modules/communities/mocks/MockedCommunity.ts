fe061085ebd63c29c9239b79f72988be1422fc8a
Wallace Soares <107143825+wallace-soares@users.noreply.github.com>
2022-09-05 15:45:48 -0300

    feat(communities): Community Selection filtering per manager (#78)
    Co-authored-by: Wallace Soares <>

--- src/modules/communities/mocks/MockedCommunity.ts
+++ src/modules/communities/mocks/MockedCommunity.ts
@@ -11,13 +11,26 @@ export const MockedFirstCommunity: Community = {
   bicycles: [],
   withdrawals: [],
 };
+
+export const MockedLoggedInUserManager: Community = {
+  address: 'Recife',
+  created_date: new Date(),
+  description: 'Comunidade em Recife',
+  id: '-MLDOXs3p35DEHg0gdUU',
+  name: 'Comunidade Gerenciada',
+  org_email: 'email@example.com',
+  org_name: 'John',
+  bicycles: [],
+  withdrawals: [],
+};
+
 export const MockedSecondCommunity: Community = {
   address: 'Rua Teste, 123',
   created_date: new Date(),
   description: 'Descricao teste',
   id: '-MLy8y1-5v5GLg7Z428y',
   name: 'Nome Teste',
-  org_email: 'orgtest@orgtest.com',
+  org_email: 'email@example.com',
   org_name: 'Nome Org Teste',
   bicycles: [],
   withdrawals: [],
