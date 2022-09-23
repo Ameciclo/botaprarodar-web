10deb55ba45d5d1455b2e461256864f2bb88c5c2
Carlos Antônio <carlos.antonio@thoughtworks.com>
2022-09-23 15:07:58 -0300

    test(custom): Bicycles

--- src/modules/bicycles/pages/ReturnBike/ReturnBikeStepOne/ReturnBikeStepOne.test.tsx
+++ src/modules/bicycles/pages/ReturnBike/ReturnBikeStepOne/ReturnBikeStepOne.test.tsx
@@ -13,13 +13,15 @@ describe('ReturnBikeStepOne', () => {
     });
 
     it('should render correctly', async () => {
-      const { history } = renderWithRouterAndAuth(<ReturnBikeStepOne />, {
-        route: '/comunidades/devolver-bicicleta/formulario',
-        stateParams: { communityId, selectedBike },
-      });
+      const { history, container } = renderWithRouterAndAuth(
+        <ReturnBikeStepOne />,
+        {
+          route: '/comunidades/devolver-bicicleta/formulario',
+          stateParams: { communityId, selectedBike },
+        },
+      );
 
       const { location } = history;
-
       const title = screen.getByRole('heading', { name: 'Voltar' });
 
       expect(location.pathname).toEqual(
@@ -29,8 +31,29 @@ describe('ReturnBikeStepOne', () => {
         communityId: 'my-community-id',
         selectedBike: '123',
       });
-
       expect(title).toBeInTheDocument();
+      expect(screen.getByText('Questionário')).toBeInTheDocument();
+      expect(screen.getByText('Responda ao questionário')).toBeInTheDocument();
+
+      expect(
+        screen.getByText('A bicicleta foi usada para que?'),
+      ).toBeInTheDocument();
+      expect(screen.getByTestId('select-bike-use-test')).toBeInTheDocument();
+
+      expect(
+        screen.getByText('Para qual bairro você foi?'),
+      ).toBeInTheDocument();
+      expect(screen.getByTestId('bike-neighborhood-test')).toBeInTheDocument();
+
+      expect(
+        screen.getByText(
+          'Você sofreu alguma violência no trânsito durante o seu trajeto? (exemplo: carro passou perto e rápido demais, foi fechada numa curva, etc)?',
+        ),
+      ).toBeInTheDocument();
+      expect(screen.getByText('Precisou levar carona?')).toBeInTheDocument();
+      expect(container.querySelectorAll(`input[type="radio"]`).length).toBe(4);
+
+      expect(screen.getByTestId('submit-button')).toBeInTheDocument();
     });
 
     it('should redirect to first page of return bike when clicking back', async () => {
