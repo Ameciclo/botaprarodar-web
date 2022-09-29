5d0bdbc153e1e1dfec15d0996f1a18929a6b04e8
Carlos Antônio <carlos.antonio@thoughtworks.com>
2022-09-29 15:48:48 -0300

    fix: No bikes found message for SelectBikePage

--- src/modules/bicycles/components/SelectBikePage/SelectBikePage.test.tsx
+++ src/modules/bicycles/components/SelectBikePage/SelectBikePage.test.tsx
@@ -17,13 +17,11 @@ describe('SelectBikePage', () => {
   describe('when has state params', () => {
     it('should render correctly', async () => {
       mockedBikeService.getBikesPerCommunity.mockResolvedValue([mockedBike()]);
-      await act(() => {
-        renderWithRouterAndAuth(
-          <BrowserRouter>
-            <SelectBikePage communityId="-MLDOXs3p35DEHg0gdUU" />
-          </BrowserRouter>,
-        );
-      });
+      renderWithRouterAndAuth(
+        <BrowserRouter>
+          <SelectBikePage communityId="-MLDOXs3p35DEHg0gdUU" />
+        </BrowserRouter>,
+      );
 
       await waitForElementToBeRemoved(() =>
         screen.getByText('Carregando, por favor aguarde...'),
@@ -33,17 +31,41 @@ describe('SelectBikePage', () => {
       expect(card).toBeInTheDocument();
     });
 
-    it('should render list of bikes', async () => {
+    it('should render list of bikes when returned by endpoint', async () => {
       mockedBikeService.getBikesPerCommunity.mockResolvedValue([mockedBike()]);
 
       await act(async () => {
-        render(
+        renderWithRouterAndAuth(
           <BrowserRouter>
             <SelectBikePage communityId="-MLDOXs3p35DEHg0gdUU" />
           </BrowserRouter>,
         );
       });
       expect(screen.getByTestId('bikeList')).toBeInTheDocument();
+      expect(screen.getByText('Selecione a bicicleta')).toBeInTheDocument();
+    });
+
+    it('should render no bikes found message', async () => {
+      mockedBikeService.getBikesPerCommunity.mockResolvedValue([]);
+
+      await act(async () => {
+        renderWithRouterAndAuth(
+          <BrowserRouter>
+            <SelectBikePage communityId="-MLDOXs3p35DEHg0gdUU" />
+          </BrowserRouter>,
+        );
+      });
+      expect(
+        screen.queryByText('Selecione a bicicleta'),
+      ).not.toBeInTheDocument();
+      expect(
+        screen.getByText('Nenhuma bicicleta disponível!'),
+      ).toBeInTheDocument();
+      expect(
+        screen.getByText(
+          'Verifique as bicicletas cadastradas e tente novamente.',
+        ),
+      ).toBeInTheDocument();
     });
   });
 });
