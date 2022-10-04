8ffd463fefffe314f2fd80bb6bbc4987e821ad5f
Carlos Antônio <carlos.antonio@thoughtworks.com>
2022-10-04 15:22:25 -0300

    fix(dashboard): Merging bike tests

--- src/modules/bicycles/pages/BikeConfirmationPage/BikeConfirmationPage.test.tsx
+++ src/modules/bicycles/pages/BikeConfirmationPage/BikeConfirmationPage.test.tsx
@@ -1,9 +1,11 @@
 import { screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { renderWithRouterAndAuth } from 'setupTests';
-import { MockedFirstUser } from 'modules/users/mocks/MockedUser';
 import { mockedBike } from 'modules/bicycles/mocks/BikeMocks';
 import BikeService from 'modules/bicycles/services/BikeService';
+import { mockedFormValues } from 'modules/bicycles/mocks/FormValuesMock';
+import { MockedFirstUser, mockedUser } from 'modules/users/mocks/MockedUser';
+import { UserService } from 'modules/users/services';
 import BikeConfirmationPage from './BikeConfirmationPage';
 
 const mockUser = MockedFirstUser;
@@ -12,22 +14,14 @@ let communityId;
 let selectedBike;
 let selectedUser;
 let historyRender;
+let formValues;
 
 jest.mock('modules/bicycles/services/BikeService');
 const mockedBikeService = BikeService as jest.Mocked<typeof BikeService>;
+jest.mock('modules/users/services/UserService');
+const mockedUserService = UserService as jest.Mocked<typeof UserService>;
 
 describe('Render Bike Confirmation Page', () => {
-  describe('without props', () => {
-    renderWithRouterAndAuth(<BikeConfirmationPage />);
-    it('should render bike card and bike user card', async () => {
-      const bikeCard = screen.getByTestId('bike-card');
-      expect(bikeCard).toBeInTheDocument();
-
-      const bikeUserCard = screen.getByTestId('bike-user-card');
-      expect(bikeUserCard).toBeInTheDocument();
-    });
-  });
-
   describe('with props', () => {
     beforeEach(() => {
       selectedBike = bikeMock;
@@ -83,6 +77,121 @@ describe('Render Bike Confirmation Page', () => {
         expect(historyRender.location.pathname).toEqual(
           '/comunidades/bicicleta/final',
         );
+      });
+    });
+  });
+
+  describe('BikeConfirmationPage', () => {
+    describe('when has state params', () => {
+      beforeEach(() => {
+        communityId = 'my-community-id';
+        selectedBike = mockedBike();
+      });
+
+      it('should render correctly for withdraw', async () => {
+        selectedUser = mockedUser();
+        const { history } = renderWithRouterAndAuth(<BikeConfirmationPage />, {
+          route: '/comunidades/emprestar-bicicleta/confirmacao',
+          stateParams: { communityId, selectedBike, selectedUser },
+        });
+
+        const { location } = history;
+
+        expect(location.pathname).toEqual(
+          '/comunidades/emprestar-bicicleta/confirmacao',
+        );
+        expect(location.state).toEqual({
+          communityId,
+          selectedBike,
+          selectedUser,
+        });
+
+        expect(screen.getByText('Voltar')).toBeInTheDocument();
+        expect(screen.getByText('Emprestar bicicleta')).toBeInTheDocument();
+        expect(screen.getByText('Confirme o empréstimo')).toBeInTheDocument();
+        expect(screen.getByTestId('bike-pic')).toBeInTheDocument();
+        expect(screen.getByTestId('bike-user-card')).toBeInTheDocument();
+        expect(screen.getByTestId('submit-button')).toBeInTheDocument();
+      });
+
+      it('should render correctly for devolution', async () => {
+        formValues = mockedFormValues();
+        mockedUserService.getUserById.mockResolvedValue(MockedFirstUser);
+        const { history } = renderWithRouterAndAuth(<BikeConfirmationPage />, {
+          route: '/comunidades/devolver-bicicleta/confirmacao',
+          stateParams: { communityId, selectedBike, formValues },
+        });
+
+        const { location } = history;
+
+        expect(location.pathname).toEqual(
+          '/comunidades/devolver-bicicleta/confirmacao',
+        );
+        expect(location.state).toEqual({
+          communityId,
+          selectedBike,
+          formValues,
+        });
+
+        await waitFor(() => {
+          expect(screen.getByText('Voltar')).toBeInTheDocument();
+          expect(screen.getByText('Devolver bicicleta')).toBeInTheDocument();
+          expect(screen.getByText('Confirme a devolução')).toBeInTheDocument();
+          expect(screen.getByTestId('bike-pic')).toBeInTheDocument();
+          expect(screen.getByTestId('bike-user-card')).toBeInTheDocument();
+          expect(screen.getByTestId('submit-button')).toBeInTheDocument();
+        });
+      });
+
+      it('should return to select bike when going back during withdraw', async () => {
+        selectedUser = mockedUser();
+        const { history } = renderWithRouterAndAuth(<BikeConfirmationPage />, {
+          route: '/comunidades/emprestar-bicicleta/confirmacao',
+          stateParams: { communityId, selectedBike, selectedUser },
+        });
+
+        userEvent.click(screen.getByText('Voltar'));
+
+        await waitFor(() =>
+          expect(history.location.pathname).toBe(
+            '/comunidades/emprestar-bicicleta/selecionar-bicicleta',
+          ),
+        );
+      });
+
+      it('should return to user quiz when going back during devolution', async () => {
+        formValues = mockedFormValues();
+        mockedUserService.getUserById.mockResolvedValue(MockedFirstUser);
+        const { history } = renderWithRouterAndAuth(<BikeConfirmationPage />, {
+          route: '/comunidades/devolver-bicicleta/confirmacao',
+          stateParams: { communityId, selectedBike, formValues },
+        });
+
+        userEvent.click(screen.getByText('Voltar'));
+
+        await waitFor(() =>
+          expect(history.location.pathname).toBe(
+            '/comunidades/devolver-bicicleta/questionario',
+          ),
+        );
+      });
+
+      describe('when does not have state params', () => {
+        it('should show empty state when having no parameters for withdraw', async () => {
+          renderWithRouterAndAuth(<BikeConfirmationPage />, {
+            route: '/comunidades/emprestar-bicicleta/confirmacao',
+          });
+          const loadingText = screen.getByText('Página não encontrada');
+          expect(loadingText).toBeInTheDocument();
+        });
+
+        it('should show empty state when having no parameters for devolution', async () => {
+          renderWithRouterAndAuth(<BikeConfirmationPage />, {
+            route: '/comunidades/devolver-bicicleta/confirmacao',
+          });
+          const loadingText = screen.getByText('Página não encontrada');
+          expect(loadingText).toBeInTheDocument();
+        });
       });
     });
   });
