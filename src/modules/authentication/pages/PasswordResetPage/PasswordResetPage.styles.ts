833559fede8da33237edd5283a5a88bc716685d6
Vitor Mascena Barbosa <vitormbarbosa@gmail.com>
2022-11-08 17:50:12 -0300

    refactor(login): Remove unused variable to fix CI build

--- src/modules/authentication/pages/PasswordResetPage/PasswordResetPage.styles.ts
+++ src/modules/authentication/pages/PasswordResetPage/PasswordResetPage.styles.ts
@@ -1,6 +1,6 @@
 import { makeStyles } from '@material-ui/core';
 
-export default makeStyles(theme => ({
+export default makeStyles(() => ({
   root: {
     backgroundColor: '#E5E5E5',
     display: 'flex',
