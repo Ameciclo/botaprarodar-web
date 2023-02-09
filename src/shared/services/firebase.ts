f0e7c423ffe5b48d97c45f51c9504753f2041d50
luansviana <luan.bcc1@gmail.com>
2023-02-09 13:59:47 -0300

    chore(bikes): Config firebase storage

--- src/shared/services/firebase.ts
+++ src/shared/services/firebase.ts
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
+import 'firebase/storage';
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -15,3 +16,4 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 export const auth = firebase.auth();
 export const database = firebase.database();
+export const storage = firebase.storage();
